df257b0fab6714009bfa7bc9a312f35eaae8e239
alexandermcmillan <mcmillancontact@protonmail.com>
2024-12-05 19:43:41 -0800

    test table up/sending restful data

--- server/app.py
+++ server/app.py
@@ -3,21 +3,37 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, jsonify, make_response, request
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
 # Add your model imports
+from models import Test
+from flask_cors import CORS
 
 
 # Views go here!
+
+
 
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
 
+class TestPage(Resource):
+    def get(self):
+        tests = Test.query.all()
+
+        return [test.to_dict() for test in tests]
+    
+api.add_resource(TestPage, '/test')
+
+
+
+
+
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+        app.run(port=5555, debug=True)
 
--- server/models.py
+++ server/models.py
@@ -1,6 +1,17 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
+
 from config import db
 
 # Models go here!
+
+class Test(db.Model, SerializerMixin):
+  __tablename__ = "Test1"
+
+  id = db.Column(db.Integer, primary_key=True)
+  test_data = db.Column(db.String, default="test1")
+  test_data2 = db.Column(db.String, default="test2")
+
+
+
