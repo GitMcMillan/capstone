88df414f0421dee995a08cbf3285cc120eec32ea
alexandermcmillan <mcmillancontact@protonmail.com>
2024-12-08 15:40:09 -0800

    tailwind working properly

--- client/src/components/BookCard.js
+++ client/src/components/BookCard.js
@@ -2,13 +2,10 @@ import React from "react";
 
 const BookCard = ({ book }) => {
   return (
-    <li className="bg-red-500 text-white text-2xl font-bold border-4 border-black shadow-lg p-6 mb-6">
+    <li className="bg-gray-100 shadow-md rounded-md p-4 mb-4">
       <p className="text-lg font-bold">Title: {book.title}</p>
       <p className="text-sm text-gray-600">Author: {book.author}</p>
       <p className="text-sm text-gray-600">Pages: {book.page_number}</p>
-      <div className="bg-blue-500 text-white p-10">
-        Tailwind working if blue
-      </div>
     </li>
   );
 };
--- client/src/components/UserCard.js
+++ client/src/components/UserCard.js
@@ -2,12 +2,9 @@ import React from "react";
 
 const UserCard = ({ user }) => {
   return (
-    <li className="bg-red-500 text-white text-2xl font-bold border-4 border-black shadow-lg p-6 mb-6">
+    <li className="bg-gray-100 shadow-md rounded-md p-4 mb-4">
       <p className="text-lg font-bold">Username: {user.username}</p>
       <p className="text-sm text-gray-600">Email: {user.email}</p>
-      <div className="bg-blue-500 text-white p-10">
-        Tailwind working if blue
-      </div>
     </li>
   );
 };
