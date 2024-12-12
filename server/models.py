1c2f18f1071486996c9f4c6014b12730c625d330
alexandermcmillan <mcmillancontact@protonmail.com>
2024-12-12 14:33:38 -0800

    models reconfigured

--- server/models.py
+++ server/models.py
@@ -2,7 +2,6 @@ from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
-from extensions import bcrypt
 from config import flask_bcrypt
 
 # Models go here!
@@ -32,61 +31,61 @@ class User(db.Model, SerializerMixin):
     def authenticate(self, password_to_check):
         return flask_bcrypt.check_password_hash(self._password_hash, password_to_check)
 
-    libraries = db.relationship('Library', back_populates='user')
+    books = db.relationship('Book', back_populates='user')
 
-    serialize_rules = ('-libraries', '-password_hash')
+    serialize_rules = ('-books', '-password_hash')
 
     def __repr__(self):
         return f'User {self.id}: {self.username}, {self.email}'
 
-class Tag(db.Model, SerializerMixin):
-    __tablename__ = "tags"
+class Bookstore(db.Model, SerializerMixin):
+    __tablename__ = "bookstores"
 
     id = db.Column(db.Integer, primary_key=True)
-    genre = db.Column(db.String, nullable=False)
-    best_seller = db.Column(db.Boolean, nullable=False)
-    fiction = db.Column(db.Boolean, nullable=False)
-    award_winner = db.Column(db.Boolean, nullable=False)
-    new_release = db.Column(db.Boolean, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    address = db.Column(db.String, nullable=False)
+    phone_number = db.Column(db.Integer, nullable=False)
+    
 
-    books = db.relationship('Book', secondary='book_tags', back_populates='tags')
+    books = db.relationship('Book', back_populates='bookstores')
+
+    serialize_rules = ('-books')
 
     def __repr__(self):
-        return f'Tags: {self.id}: {self.genre}, {self.best_seller}, {self.fiction}, {self.award_winner}, {self.new_release}'
+        return f'Bookstores: {self.id}: {self.name}, {self.address}, {self.phone_number}'
 
-book_tags = db.Table(
-    'book_tags',
-    db.Column('book_id', db.Integer, db.ForeignKey('books.id'), primary_key=True),
-    db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'), primary_key=True)
-)
+
+
+class Author(db.Model, SerializerMixin):
+    __tablename__ = 'authors'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    
+
+    books = db.relationship('Book', back_populates='author')
+    
+
+    serialize_rules = ('-books')
+
+    def __repr__(self):
+        return f'Author: {self.id}:{self.name}'
 
 class Book(db.Model, SerializerMixin):
-    __tablename__ = 'books'
+    __tablename__ = "books"
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
-    author = db.Column(db.String, nullable=False)
     page_number = db.Column(db.Integer, nullable=False)
-
-    libraries = db.relationship('Library', back_populates='book')
-    tags = db.relationship('Tag', secondary='book_tags', back_populates='books')
-
-    serialize_rules = ('-libraries', '-tags')
-
-    def __repr__(self):
-        return f'Book: {self.id}:{self.title}, {self.author}, {self.page_number}'
-
-class Library(db.Model, SerializerMixin):
-    __tablename__ = "libraries"
-
-    id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    book_id = db.Column(db.Integer, db.ForeignKey('books.id'))
+    bookstore_id = db.Column(db.Integer, db.ForeignKey('bookstores.id'))
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'))
 
-    user = db.relationship('User', back_populates='libraries')
-    book = db.relationship('Book', back_populates='libraries')
+    user = db.relationship('User', back_populates='books')
+    author = db.relationship('Author', back_populates='books')
+    bookstore = db.relationship('Bookstore', back_populates='books')
 
-    serialize_rules = ('-user.libraries', '-book.libraries')
+    serialize_rules = ('-user.books', '-bookstore.books', 'author.books')
 
     def __repr__(self):
-        return f'Library {self.id}: User {self.user_id}, Book {self.book_id}'
+        return f'Book {self.id}: User: {self.user_id}, Bookstore: {self.bookstore_id}, Author: {self.author_id}'
